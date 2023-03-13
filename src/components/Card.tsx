8d190ed9b7377c850d860f4a3aa3eea9e6488e11
Siarhei Melekh <sergeymelekh95@gmail.com>
2023-03-13 14:21:39 +0400

    fix/cardMedia

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,14 +9,6 @@ import { CardHeader } from '@mui/material';
 import { Item } from '../models';
 import { Box } from '@mui/system';
 import { addZero } from '../helpers';
-import { styled } from '@mui/material/styles';
-
-const ImgStyled = styled(CardMedia)(({ theme }) => ({
-	...theme.typography.body2,
-	width: 70,
-	height: 70,
-}));
-
 interface IMyCard {
 	item: Item;
 }
@@ -30,8 +22,6 @@ export const MyCard: React.FC<IMyCard> = ({ item }) => {
 	const day: number = date.getDay();
 	const hours: number = date.getHours();
 	const minutes: number = date.getMinutes();
-
-	console.log(date);
 
 	return (
 		<Card>
@@ -48,13 +38,17 @@ export const MyCard: React.FC<IMyCard> = ({ item }) => {
 					alignItems: 'center',
 				}}
 			>
-				<ImgStyled
+				<CardMedia
+					sx={{ width: 70, height: 70 }}
+					component='img'
 					image={`https://footballista.ru/api/img/logos/${teamHome.logo}-middle.png?logoId=${teamHome.logoId}`}
 				/>
 				<Box style={{ textAlign: 'center' }}>
 					<Typography>-</Typography>
 				</Box>
-				<ImgStyled
+				<CardMedia
+					sx={{ width: 70, height: 70 }}
+					component='img'
 					image={`https://footballista.ru/api/img/logos/${teamAway.logo}-middle.png?logoId=${teamAway.logoId}`}
 					title='green iguana'
 				/>
